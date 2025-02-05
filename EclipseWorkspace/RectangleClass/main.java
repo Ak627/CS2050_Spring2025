0243b97f4c71ebe20ac0739154c44c6a85009916
Ak627 <alexkindall7@gmail.com>
2025-02-05 12:13:53 -0700

    Added Rectangle 2 and 3 print statements

--- EclipseWorkspace/RectangleClass/main.java
+++ EclipseWorkspace/RectangleClass/main.java
@@ -6,3 +6,5 @@ public class main:
 	RectangleClass Rectangle3 = new SimpleRectangle(-1, 4.2);
 	
 	System.out.println("Area of Rectangle 1: ", + ,Rectangle1.getArea(), + ," Perimeter of Rectangle 1: ", +, Rectangle1.getPerimeter());
+	System.out.println("Area of Rectangle 2: ", + ,Rectangle2.getArea(), + ," Perimeter of Rectangle 2: ", +, Rectangle2.getPerimeter());
+	System.out.println("Area of Rectangle 3: ", + ,Rectangle3.getArea(), + ," Perimeter of Rectangle 3: ", +, Rectangle3.getPerimeter());
