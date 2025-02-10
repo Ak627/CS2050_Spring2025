608f8db0b1463c66c5e4a23df31ab36c1061878e
Ak627 <alexkindall7@gmail.com>
2025-02-10 12:53:21 -0700

    Created Animal Superclass

---
+++ EclipseWorkspace/AnimalClasses/KindallGE01Polymorphism.java
@@ -0,0 +1,43 @@
+class Animal{
+	private String name;
+	private String food;
+	private int weight;
+	private int sleep;
+	private String location;
+	
+	public Animal(String name, String food, int weight, int sleep, String location) { // constructor
+		this.name = name;
+		this.food = food;
+		this.weight = weight;
+		this.sleep = sleep;
+		this.location = location;
+	}
+	
+	//getters, no need for setters in this code
+	public String GetName() {
+		return this.name;
+	}
+	public String GetFood() {
+		return this.food;
+	}
+	public int GetWeight() {
+		return this.weight;
+	}
+	public int GetSleep() {
+		return this.sleep;
+	}
+	public String GetLocation() {
+		return this.location;
+	}
+	
+	//general methods for animals
+	public void eat() {
+		System.out.println("Animal eats.");
+	}
+	public void sleep() {
+		System.out.println("Animal sleeps");
+	}
+	public void swim() {
+		System.out.println("Animal swims");
+	}
+}
