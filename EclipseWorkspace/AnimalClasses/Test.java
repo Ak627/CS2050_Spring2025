f6802bf77ba03288c4d8cd87423d93a75341f820
Ak627 <alexkindall7@gmail.com>
2025-02-12 12:40:32 -0700

    Added exception catching and checks for the Animal Text file

--- EclipseWorkspace/AnimalClasses/Test.java
+++ EclipseWorkspace/AnimalClasses/Test.java
@@ -1,5 +1,22 @@
+import java.io.FileNotFoundException; 
+import java.io.FileReader; 
+import java.io.IOException; 
+
 public class Test{
-	public static void main(String [] args) {
+	public static void main(String [] args) throws IOException, FileNotFoundException{
 		
+		
+		// check if File exists or not 
+        FileReader AnimFile =null; 
+        try
+        { 
+            AnimFile = new FileReader("Animals"); 
+        } 
+        catch (FileNotFoundException fe) 
+        { 
+            System.out.println("File not found"); 
+        } 
+        
+        AnimFile.close();
 	}
 }
