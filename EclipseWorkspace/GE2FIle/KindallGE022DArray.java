0bf46dbfe5b380187e14c5af268a1031ed0d65d4
Ak627 <alexkindall7@gmail.com>
2025-03-12 17:13:05 -0600

    Update KindallGE022DArray.java

--- EclipseWorkspace/GE2FIle/KindallGE022DArray.java
+++ EclipseWorkspace/GE2FIle/KindallGE022DArray.java
@@ -8,13 +8,31 @@ import java.text.DecimalFormat;
 public class KindallGE022DArray{
   public static void main(String[] args){
 	  Car[][] cars = new Car[2][3];
+	  Scanner input = new Scanner(System.in);
+	  for(int i =0; i < cars.length; i ++) {
+		  for(int j = 0; j< cars[i].length; j ++) {
+			  String make = input.nextLine();
+			  cars[i][j] = new Car(make);
+			  
+		  }
+		  
+	  }
+	  for(int i =0; i < cars.length; i ++) {
+		  for(int j = 0; j< cars[i].length; j ++) {
+			  cars[i][j].printMake();  
+		  }
+	  }
+	  
+	  input.close();
   }
   
-  class Car{
-		private String make;
+  
+}
+class Car{
+	private String make;
 
-		public Car() { 
-	   this.make = "Unknown"; 
+	public Car() { 
+		this.make = "Unknown"; 
 	}
 		
 	public Car(String make) {
@@ -25,4 +43,3 @@ public class KindallGE022DArray{
 	   System.out.print(this.make + " ");
 	}
 	} // Car
-}
