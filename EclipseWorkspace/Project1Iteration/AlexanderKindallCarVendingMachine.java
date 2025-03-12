87cafa4df0acd36ba5371b40715702e5005545a4
Ak627 <alexkindall7@gmail.com>
2025-03-12 12:16:55 -0600

    Create AlexanderKindallCarVendingMachine.java

---
+++ EclipseWorkspace/Project1Iteration/AlexanderKindallCarVendingMachine.java
@@ -0,0 +1,15 @@
+public class AlexanderKindallCarVendingMachine{
+
+public static void main(String [] args) {
+	System.out.println("hello world!");
+}
+
+
+
+public class Car{
+	public Car() {
+		System.out.println("Car Created!");		
+	}
+
+}
+}
