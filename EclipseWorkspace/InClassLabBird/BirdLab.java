54bb54d0cb44904d537acb466218c3f975d35228
Ak627 <alexkindall7@gmail.com>
2025-03-12 13:03:31 -0600

    Update BirdLab.java

--- EclipseWorkspace/InClassLabBird/BirdLab.java
+++ EclipseWorkspace/InClassLabBird/BirdLab.java
@@ -8,10 +8,15 @@ public class BirdLab{
 		public void swimTrue();
 	}
 	abstract class Bird{
+		private String type;
+		private String name;
+		private int swimSpeed;
 		
-		
-		abstract void nameType();
 		abstract void funFact();
+		@Override
+		public String toString(){
+			return type + " " + name + " " + swimSpeed;
+		}
 	}
 	
 	
