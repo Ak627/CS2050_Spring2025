75371f729fe04b60f6aab8878f22acd67d9bdea2
Ak627 <alexkindall7@gmail.com>
2025-03-12 12:25:15 -0600

    Update AlexanderKindallCarVendingMachine.java

--- EclipseWorkspace/Project1Iteration/AlexanderKindallCarVendingMachine.java
+++ EclipseWorkspace/Project1Iteration/AlexanderKindallCarVendingMachine.java
@@ -7,9 +7,30 @@ public static void main(String [] args) {
 
 
 public class Car{
-	public Car() {
-		System.out.println("Car Created!");		
+	private String CarBrand;
+	private String CarType;
+	private double price;
+	private int year;
+	public Car(String CarBrand, String CarType, double price, int year) {
+		this.CarBrand = CarBrand;
+		this.CarType = CarType;
+		this.price = price;
+		this.year = year;
 	}
+	
+	public String getBrand() { return CarBrand; }
+	public String getType() { return CarType; }
+	public double getPrice() { return price; }
+	public int getYear() { return year; }
 
+}
+
+public class VendingMachine{
+	private Car cars[];
+	public VendingMachine(Car cars[]) {
+		this.cars = cars;
+	}
+	
+	
 }
 }
