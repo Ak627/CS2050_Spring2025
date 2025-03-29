2d3df4213b7b36bdd4481857610835b61c13c944
Ak627 <alexkindall7@gmail.com>
2025-03-28 23:07:57 -0600

    Update AlexanderKindallCarVendingMachine.java

--- EclipseWorkspace/Project1Iteration/AlexanderKindallCarVendingMachine.java
+++ EclipseWorkspace/Project1Iteration/AlexanderKindallCarVendingMachine.java
@@ -1,33 +1,72 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class AlexanderKindallCarVendingMachine{
 
-public static void main(String [] args) throws FileNotFoundException{
+public static void main(String [] args) throws FileNotFoundException, InputMismatchException{
 	int fileChoice = 1;
-	VendingMachine VM = new VendingMachine();
-	Scanner fileScanner = new Scanner(new File("cars" + fileChoice +".txt"));
-	while (fileScanner.hasNextLine()) {
-		int x = Integer.parseInt(fileScanner.next());
-		int y = Integer.parseInt(fileScanner.next());
-		fileScanner.nextLine();
-		String brand = fileScanner.next();
-		String type = fileScanner.next();
-		int year = Integer.parseInt(fileScanner.next());
-		fileScanner.nextLine();
-		double price = Double.parseDouble(fileScanner.next());
-		fileScanner.nextLine();
-        VM.addCar(new Car(x,y, brand, type, year, price));
-    }
-	fileScanner.close();
+	Scanner UserInput = new Scanner(System.in);
+	System.out.println("Enter the number of floors for the car vending machine: ");
+	int rows = UserInput.nextInt();
+	System.out.println("Enter the number of spaces for the car vending machine: ");
+	int columns = UserInput.nextInt();
+	
+	VendingMachine VM = new VendingMachine(rows,columns);
+	
 	boolean exit = false;
 	while(exit != true) {
-		
+		System.out.println("=== Car Vending Machine ===");
+		System.out.println("1. Load Car Data");
+		System.out.println("2. Display Vending Machine");
+		System.out.println("3. Retrieve a Car");
+		System.out.println("4. Print Sorted Inventory (Price)");
+		System.out.println("5. Print Sorted Inventory (Year)");
+		System.out.println("6. Exit");
+		System.out.println("Enter your Choice: ");
+		int choice = UserInput.nextInt();
+		switch(choice) {
+		case 1:
+			UserInput.nextLine();
+			System.out.println("Enter the file name: ");
+			String fileName = UserInput.nextLine();
+			Scanner fileScanner = new Scanner(new File(fileName));
+			while (fileScanner.hasNextLine()) {
+				int x = Integer.parseInt(fileScanner.next());
+				int y = Integer.parseInt(fileScanner.next());
+				fileScanner.nextLine();
+				String brand = fileScanner.next();
+				String type = fileScanner.next();
+				int year = Integer.parseInt(fileScanner.next());
+				fileScanner.nextLine();
+				double price = Double.parseDouble(fileScanner.next());
+				fileScanner.nextLine();
+		        VM.addCar(new Car(x,y, brand, type, year, price));
+		    }
+			fileScanner.close();
+		case 2:
+			VM.showVendingMachine();
+		case 3:
+			System.out.println("Where would you like to retrieve the car from?");
+			int posX = UserInput.nextInt();
+			int posY = UserInput.nextInt();
+			UserInput.nextLine();
+			VM.retrieveCar(posX, posY);
+		case 4:
+			VM.sortCar(1);
+		case 5:
+			VM.sortCar(0);
+		case 6:
+			System.out.println("GoodBye!");
+			exit = true;
+		default:
+			System.out.println("Invalid input! Try again!");
+			
+		}
 	}
+	UserInput.close();
 }
-
-
 
 public static class Car{
 	private String CarBrand;
@@ -54,8 +93,8 @@ public static class Car{
 
 public static class VendingMachine{
 	private Car floors[][];
-	public VendingMachine() {
-		System.out.println("Vending Machine Created");
+	public VendingMachine(int x, int y) {
+		floors = new Car[x][y];
 	}
 	public void addCar(Car carObj) {
 		if (floors[carObj.x][carObj.y] == null){
@@ -77,7 +116,7 @@ public static class VendingMachine{
 		}
 		
 	}
-	
+
 	public void showVendingMachine() {
 		for(int i = 0; i < floors.length; i ++) {
 			System.out.println("Floor " + i+1 + ":");
